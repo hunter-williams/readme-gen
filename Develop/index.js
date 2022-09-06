1b210564a0954dfd5a8f8e2190b1c2f6c87351ad
hunter-williams <dragynson@yahoo.com>
2022-09-06 14:06:38 -0700

    notes and inital question setup

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,77 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
+const fs = require('fs');
+const { default: Choices } = require('inquirer/lib/objects/choices');
+// import helper functions from file generateMarkdown
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        name:  'title',
+        type: 'input',
+        message: 'What is the tile of your project?'
+    },
+    {
+        name:  'description',
+        type: 'input',
+        message: 'What is the description of your project?'
+    },
+    {
+        name:  'install',
+        type: 'input',
+        message: 'What are the installation instructions?'
+    },
+    {
+        name:  'usage',
+        type: 'input',
+        message: 'What are the instruction for your project and an example of its use?'
+    },
+    {
+        name:  'license',
+        type: 'checkbox',
+        message: 'What license is the project covered under',
+        choices: ['mit','ISC']
+    },
+    {
+        name:  'contribution',
+        type: 'input',
+        message: 'What are the contribution guidelines'
+    },
+    {
+        name:  'test',
+        type: 'input',
+        message: 'How do you test this code?'
+    },
+    {
+        name:  'github',
+        type: 'input',
+        message: 'What is your github?'
+    },
+    {
+        name:  'email',
+        type: 'input',
+        message: 'What is your email?'
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+    // send answers(data) to generate markdown to format as readme.md
+    fs.writeFile(fileName, generateMarkdown(data), (error, data) => {
+        console.log("WriteToFile() error",  error);
+        console.log("WriteToFile() data",  data);
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)              // user answers all the questions
+    .then((answers) => {                 // then handle answers
+        writeToFile("README.md", answers)   // answers get sent to generatemarkdown ^^^ and formateed into a readme
+    })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,48 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  //if data.liscence === "mit" return mit icon link
+    // [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  
+  //if data.liscence === "isc" return isc icon link
+    // [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+  
+  // else " "
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  // same as render icon but link to liscence not icon
+    // https://opensource.org/licenses/MIT
+    // https://opensource.org/licenses/ISC
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  // if liscence return description of what liscence the application is covered under?
+  // else " "
+
+  //THEN a badge for that license is added near the top of the README 
+  //and a notice is added to the section of the README entitled License 
+  //that explains which license the application is covered under               <<<<
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description
+  - ${data.description}
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - []()
+
+  ## Installation
+  - ${}
 
 `;
 }
